666e9b4e149bc966a6ca882071a28927cd086b47
sonmh79 <sonmh79@naver.com>
2021-04-22 14:12:21 +0900

    left-side

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ var audio = document.querySelector("audio");
 var pause = document.querySelector(".pause");
 var play = document.querySelector(".play");
 var reset = document.querySelector(".reset");
+var button =document.querySelector(".entry");
 
-button =document.querySelector("button");
 
-play.addEventListener("click",function(){
-    audio.play()
-})
-pause.addEventListener("click",function(){
-    audio.pause()
-})
-reset.addEventListener("click",function(){
-    audio.pause()
-    audio.currentTime=0;
-})
-button.addEventListener("click",function(){
-    audio.play()
-    audio.volume = 0.3;
-    var splashScreen = document.querySelector(".splash-screen");
-    splashScreen.classList.add("splash");
-});
+function init() {
+    play.addEventListener("click",function(){
+        audio.play();
+    })
+    pause.addEventListener("click",function(){
+        audio.pause();
+    })
+    reset.addEventListener("click",function(){
+        audio.pause();
+        audio.currentTime=0;
+    })
+    button.addEventListener("click",function(){
+        var splashScreen = document.querySelector(".splash-screen");
+        splashScreen.classList.add("splash");
+        audio.play();
+        audio.volume = 0.3;
+    });
+    
+}
+//init();
