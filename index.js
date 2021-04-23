cae0a119c7bf337609e0ea738603520657ffd25e
sonmh79 <sonmh79@naver.com>
2021-04-23 15:32:56 +0900

    update..

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 var audio = document.querySelector("audio");
-var pause = document.querySelector(".pause");
 var play = document.querySelector(".play");
 var reset = document.querySelector(".reset");
 var button =document.querySelector(".entry");
+var pause = document.querySelector(".pause");
 
 
 function init() {
-    play.addEventListener("click",function(){
-        audio.play();
-    })
-    pause.addEventListener("click",function(){
-        audio.pause();
-    })
-    reset.addEventListener("click",function(){
-        audio.pause();
-        audio.currentTime=0;
-    })
-    button.addEventListener("click",function(){
+    button.addEventListener("click",function(e){
+        e.preventDefault();
         var splashScreen = document.querySelector(".splash-screen");
         splashScreen.classList.add("splash");
         audio.play();
         audio.volume = 0.3;
-    });
-    
+    })
+    play.addEventListener("click",function(e){
+        e.preventDefault();
+        audio.play();
+    })
+    pause.addEventListener("click",function(e){
+        e.preventDefault();
+        audio.pause();
+    })
+    reset.addEventListener("click",function(e){
+        e.preventDefault();
+        audio.pause();
+        audio.currentTime=0;
+    })
 }
-//init();
+init();
