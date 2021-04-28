0353a59e7c0c2b4a85d0645e7bc398d0949a5d94
sonmh79 <sonmh79@naver.com>
2021-04-29 02:27:46 +0900

    update

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 var audio = document.querySelector("audio");
 var play = document.querySelector(".play");
 var reset = document.querySelector(".reset");
-var button =document.querySelector(".entry");
+var button = document.querySelector(".entry");
 var pause = document.querySelector(".pause");
 
 
 function init() {
+    play.addEventListener("click",function(e){
+        console.log("play")
+        e.preventDefault();
+        audio.play();
+    });
+    pause.addEventListener("click",function(e){
+        e.preventDefault();
+        console.log("pause")
+        audio.pause();
+    });
+    reset.addEventListener("click",function(e){
+        e.preventDefault();
+        console.log("reset")
+        audio.pause();
+        audio.currentTime=0;
+    });
     button.addEventListener("click",function(e){
         e.preventDefault();
         var splashScreen = document.querySelector(".splash-screen");
         splashScreen.classList.add("splash");
         audio.play();
         audio.volume = 0.3;
-    })
-    play.addEventListener("click",function(e){
-        e.preventDefault();
-        audio.play();
-    })
-    pause.addEventListener("click",function(e){
-        e.preventDefault();
-        audio.pause();
-    })
-    reset.addEventListener("click",function(e){
-        e.preventDefault();
-        audio.pause();
-        audio.currentTime=0;
-    })
+    });
 }
 init();
